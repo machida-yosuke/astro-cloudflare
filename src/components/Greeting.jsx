c6300b7bcc4f3c6b04814b2b831409032f8ba6e3
machida-yosuke <0fz3394278f245t@gmail.com>
2025-05-12 12:59:48 +0900

    state

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Greeting({ name }) {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Hello, {name}!</h1>
       <p>Welcome to our Astro + React site!</p>
+
+      <div>
+        <p>Current count: {count}</p>
+        <button onClick={() => setCount(count + 1)} type="button">Increment</button>
+      </div>
     </div>
   );
 }
